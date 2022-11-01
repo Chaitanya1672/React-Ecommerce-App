d302f2f019372e1eef5311caf2031bf20ed0a823
Chaitanya Narhare <chetannarhare111@gmail.com>
2022-11-01 22:53:26 +0530

    Added slider component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import SearchIcon from "@mui/icons-material/Search";
+import { Badge } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Navbar() {
   return (
@@ -10,13 +12,22 @@ function Navbar() {
           <div className={styles.linkOneText}>En</div>
           <div className={styles.searchInput}>
             <input className={styles.inp} type="text" />
-            <SearchIcon className="" style={{ fontSize: "16px" }} />
+            <SearchIcon
+              className="cursor-pointer"
+              style={{ fontSize: "16px" }}
+            />
           </div>
         </div>
         <div className={styles.link2CenterDiv}>
           <div className="logo font-bold text-lg">Summer Kings</div>
         </div>
-        <div>3</div>
+        <div className={styles.link2EndDiv}>
+          <div className={styles.endDivtxt}>Register</div>
+          <div className={styles.endDivtxt}>Sign In</div>
+          <Badge badgeContent={2} color="primary" className={styles.endDivtxt}>
+            <ShoppingCartIcon />
+          </Badge>
+        </div>
       </div>
     </nav>
   );
